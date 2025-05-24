f59cc8c338f57c563fac5546f2c7d059a2dee0bb
kessius00 <kes.olling00@gmail.com>
2025-05-25 00:36:49 +0200

    delete images

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import './styles/reset.css';
 import './styles/styles.scss';
 
 // importing images
-import cloudIncoming from './img/cloudincoming.webp';
-import thunder from './img/thunder.webp';
-import sunnyClouds from './img/sunnyclouds.webp';
 import clouds from './img/clouds.webp';
 
 import { getWeatherData } from './api.js';
